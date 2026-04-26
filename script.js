db934bf4ce65fc4bb3ae12ee22d81a1dc3dba918
ProfeJoseArcenio <profejosearcenio@gmail.com>
2026-04-26 17:00:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var contenedor = document.getElementById("tabla-container");
+  contenedor.textContent = "Próximamente: criterios de evaluación GTC 185.";
+});
